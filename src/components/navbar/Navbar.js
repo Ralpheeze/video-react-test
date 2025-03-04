a8fbc98ab22aa2cc3d96b0987a75da23343b5071
RAPHAEL EZE <ifeanyichukwu.eze@stu.cu.edu.ng>
2025-03-04 12:12:11 -0800

    fixed the check in grid, fixed the umbrella text field on the hero section, removed the green background color on rooms and suites

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div>
         <nav class="navbar navbar-expand-lg  shadow-5-strong my-nav-bar">
-            <a class="navbar-brand text-white navTitle" href="#"><i class="fa fa-umbrella fa-flip nav-icon"></i>Umbrella 
+            <a class="navbar-brand text-white navTitle" href="#"><i class="fa fa-umbrella fa-flip nav-icon"></i> Umbrella 
             <br /> Academy</a>
 
             <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
